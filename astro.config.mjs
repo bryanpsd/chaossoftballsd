94039e9d560078229cc9019fb15b1f759234c530
Bryan P <bryanpsd@gmail.com>
2025-09-16 12:24:29 -0700

    Testing build

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import robotsTxt from "astro-robots-txt";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://chaossoftballsd.org",
+	output: "server",
 	adapter: netlify(),
 	integrations: [
 		react(),

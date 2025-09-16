60480f677d691210833d0a76f80e4cb64ad144a9
Bryan P <bryanpsd@gmail.com>
2025-09-16 10:32:10 -0700

    Testing build

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,6 @@ import robotsTxt from "astro-robots-txt";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://chaossoftballsd.org",
-	output: "server",
 	integrations: [
 		react(),
 		partytown({ config: { forward: ["dataLayer.push"] } }),

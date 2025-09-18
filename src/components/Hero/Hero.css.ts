6d2331125b49534616161a893759f0e942f21058
Bryan <bryanpsd@gmail.com>
2025-09-18 12:27:02 -0700

    Cleaning up site

---
+++ src/components/Hero/Hero.css.ts
@@ -0,0 +1,52 @@
+import { globalStyle, style } from "@vanilla-extract/css";
+
+export const hero = style({
+	position: "relative",
+	display: "flex",
+	alignItems: "center",
+	flexDirection: "column",
+	justifyContent: "center",
+	padding: 16,
+	minHeight: "60vh",
+	overflow: "hidden",
+	textAlign: "center",
+	marginBottom: 24,
+	selectors: {
+		"&#hero::before": {
+			content: "''",
+			position: "absolute",
+			top: 0,
+			left: 0,
+			width: "100%",
+			height: "100%",
+			background: "black",
+			opacity: 0.7,
+			zIndex: 0,
+			pointerEvents: "none",
+		},
+	},
+});
+
+globalStyle(`#hero > *:not(#background)`, {
+	position: "relative",
+	zIndex: 1,
+});
+
+export const background = style({
+	position: "absolute",
+	top: 0,
+	left: 0,
+	width: "100%",
+	height: "100%",
+	objectFit: "cover",
+	zIndex: -1,
+	pointerEvents: "none",
+	userSelect: "none",
+});
+
+globalStyle(`#faqs`, {
+	marginTop: "2rem",
+	display: "flex",
+	justifyContent: "center",
+	flexDirection: "column",
+});
